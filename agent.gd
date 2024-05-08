e0679b5cfdb742002b16b27a11c380e902ecbb00
PDM52 <piotrskiera52@gmail.com>
2024-05-08 16:02:15 +0200

    Adding neural network

--- agent.gd
+++ agent.gd
@@ -4,23 +4,28 @@ extends Node
 @onready var repceptor_2 = $Receptor_2
 @onready var repceptor_3 = $Receptor_3
 
+var Network = preload("res://neuronNetwork.gd").NeuralNetwork
+
 var speed = 1
 var rot = randf()
 var noise1 = FastNoiseLite.new()
 var noise2 = FastNoiseLite.new()
 var time = randf()
 var energy = 300
+var network = Network.new(4, 2, [2,2])
 
 func _ready():
 	noise1.seed = randi()
 	noise2.seed = randi()
 
 func _process(delta):
-	speed = noise1.get_noise_1d(time)
+	var output = network.get_output([int(repceptor_1.is_colliding()), int(repceptor_2.is_colliding()), int(repceptor_3.is_colliding()), energy/1000])
+	speed = output[0]
+	print(network.get_output([time, energy]))
 	var velocity = Vector2.UP.rotated(self.rotation) * abs(speed) * 10
 	energy -= abs(speed)
 	self.position += velocity
-	rot = noise2.get_noise_1d(time) * 2 * PI
+	rot = output[1] * 2 * PI
 	time += delta
 	self.rotation = rot
 	if energy <= 0:
---
+++ neuronNetwork.gd
@@ -0,0 +1,67 @@
+extends Node
+
+class Neuron:
+	var weights: Array
+	var bias: float
+
+	func _init(input_size: int):
+		weights = []
+		for i in range(input_size):
+			weights.append(randf() * 2 - 1)
+			bias = randf() * 2 - 1 
+
+	func activate(inputs: Array) -> float:
+		var sum = bias
+		for i in range(inputs.size()):
+			sum += inputs[i] * weights[i]
+		return sigmoid(sum)
+
+	func sigmoid(x: float) -> float:
+		return 1.0 / (1.0 + exp(-x))
+		
+	func _to_string():
+		return str(weights)
+
+class Layer:
+	var input_size: int
+	var neurons: Array = []
+
+	func _init(input_size: int, neuron_count: int):
+		input_size = input_size
+		for i in range(neuron_count):
+			neurons.append(Neuron.new(input_size))
+
+	func feed_forward(inputs: Array) -> float:
+		return neurons[0].activate(inputs)
+		
+	func get_outputs(input: Array) -> Array:
+		var outputs = []
+		for neuron in neurons:
+			outputs.append(neuron.activate(input))
+		return outputs
+		
+	func _to_string():
+		return str(neurons)
+
+class NeuralNetwork:
+	var input_size: int
+	var layers: Array = []
+
+	func _init(input_size: int, ouput_size: int, hiden_size: Array):
+		input_size = input_size
+		var layer_sizes = hiden_size
+		layer_sizes.append(ouput_size)
+		for neuron_count in layer_sizes:
+			layers.append(Layer.new(input_size, neuron_count))
+			input_size = neuron_count
+		print(layers)
+	
+	func get_output(input: Array) -> Array:
+		var output
+		for layer in layers:
+			output = layer.get_outputs(input)
+			input = output
+		return output
+
+	func feed_forward(inputs: Array) -> float:
+		return layers[0].activate(inputs)
